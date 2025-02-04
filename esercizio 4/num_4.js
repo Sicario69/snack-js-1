903c0075c593b7225ff90fb536c826bb6daac2ec
Sicario69 <emanuele.fallucchi@gmail.com>
2025-02-04 11:55:26 +0100

    fatto esercizio 4

---
+++ esercizio 4/num_4.js
@@ -0,0 +1,11 @@
+const persona = {
+    nome: "Gino",
+    cognome: "Panino",
+    età: 20,
+    hobby: ["sport", "videoiochi"]
+}
+
+const {nome, età} = persona
+
+console.log (nome)
+console.log (età)
