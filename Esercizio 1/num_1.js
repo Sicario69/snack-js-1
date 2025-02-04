268223df648454fefd55b7e57efa22bc78853e00
Sicario69 <emanuele.fallucchi@gmail.com>
2025-02-04 11:32:52 +0100

    primo esercizio

---
+++ Esercizio 1/num_1.js
@@ -0,0 +1,8 @@
+const persone = [
+    {nome: "Gino", età: 25},
+    {nome:"Pino", età: 30},
+    {nome:"Luca", età: 22}
+];
+ persone.forEach ((persona) => {
+       console.log (`${persona.nome} ha ${persona.età} anni` )
+ })
